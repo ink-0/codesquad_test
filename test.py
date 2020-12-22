4a19cfe822d0a3efb1124efedbfd83ea6045b4f4
1nk_0 <dksud27@naver.com>
2020-12-22 17:41:20 +0900

    STEP-2 평면큐브
    B,R,L 새로 만들어 작동되는 지 test

--- test.py
+++ test.py
@@ -7,6 +7,19 @@ def U(cube,type): # U 테스트
         cube[0].append(cube[0].pop(0)) # left 알고리즘 적용
     return cube
 
+# B,R,L 함수생성
+def B(cube, type):
+    print('B')
+    return cube
+
+def R(cube, type):
+    print('R')
+    return cube
+
+def L(cube, type):
+    print('L')
+    return cube
+
 cube = [ ['R','R','W'],['G','C','W'],['G','B','B'] ]
 for i in cube:
     print(*i)
@@ -14,3 +27,8 @@ print()
 
 print(U(cube,'right')) # U -> R 테스트
 print(U(cube,'left')) # U -> L 테스트
+
+# B,R,L 함수 작동확인
+print(R(cube,'type'))
+print(L(cube,'type'))
+print(B(cube,'type'))
