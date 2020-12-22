34f2448022421d993a22d9df2f1ba7196e6d6e2b
1nk_0 <dksud27@naver.com>
2020-12-22 17:38:56 +0900

    STEP-2 평면큐브
    U 테스트2

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 #step-2 평면큐브  테스트코드
 #U 테스트
-def U(cube,type):
+def U(cube,type): # U 테스트
     if type=='right':
-        print('U에서 right 호출')
+        cube[0].insert(0,cube[0].pop()) # right 알고리즘 적용
     elif type=='left':
-        print('U에서 left 호출')
+        cube[0].append(cube[0].pop(0)) # left 알고리즘 적용
     return cube
 
 cube = [ ['R','R','W'],['G','C','W'],['G','B','B'] ]
@@ -12,5 +12,5 @@ for i in cube:
     print(*i)
 print()
 
-U(cube,'right') # right호출 테스트
-U(cube,'left') # left호출 테스트
+print(U(cube,'right')) # U -> R 테스트
+print(U(cube,'left')) # U -> L 테스트
