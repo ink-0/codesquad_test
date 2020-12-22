e3f05f8b2cce46fb61dbaadc7467b6187a0cca65
1nk_0 <dksud27@naver.com>
2020-12-22 17:43:33 +0900

    STEP-2 평면큐브
    U,B,R,L모든 함수 잘 작동 되는지 확인

--- test.py
+++ test.py
@@ -7,17 +7,37 @@ def U(cube,type): # U 테스트
         cube[0].append(cube[0].pop(0)) # left 알고리즘 적용
     return cube
 
-# B,R,L 함수생성
+
+# B,R,L 함수에 큐브변경 알고리즘 적용
 def B(cube, type):
-    print('B')
+    if type=='left':
+        cube[2].append(cube[2].pop(0))
+    elif type=='right':
+        cube[2].insert(0,cube[2].pop())
     return cube
-
 def R(cube, type):
-    print('R')
+    if type=='up':
+        temp=cube[0][2]
+        cube[0][2]=cube[1][2]
+        cube[1][2]=cube[2][2]
+        cube[2][2]=temp
+    elif type=='down':
+        temp=cube[2][2]
+        cube[2][2]=cube[1][2]
+        cube[1][2]=cube[0][2]
+        cube[0][2]=temp
     return cube
-
 def L(cube, type):
-    print('L')
+    if type=='up':
+        temp=cube[0][0]
+        cube[0][0]=cube[1][0]
+        cube[1][0]=cube[2][0]
+        cube[2][0]=temp
+    elif type=='down':
+        temp=cube[2][2]
+        cube[2][0]=cube[1][0]
+        cube[1][0]=cube[0][0]
+        cube[0][0]=temp
     return cube
 
 cube = [ ['R','R','W'],['G','C','W'],['G','B','B'] ]
@@ -28,7 +48,12 @@ print()
 print(U(cube,'right')) # U -> R 테스트
 print(U(cube,'left')) # U -> L 테스트
 
-# B,R,L 함수 작동확인
-print(R(cube,'type'))
-print(L(cube,'type'))
-print(B(cube,'type'))
+# B,R,L 함수 작동테스트
+print(R(cube,'up'))
+print(R(cube,'down'))
+print()
+print(L(cube,'up'))
+print(L(cube,'down'))
+print()
+print(B(cube,'right'))
+print(B(cube,'left'))
